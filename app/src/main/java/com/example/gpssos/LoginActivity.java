baa14e79fe7774207e395ce787b06b28c7e09644
ivanna <itorres13@ucol.mx>
2023-08-26 12:00:48 -0600

    Inicio de sesión terminado

--- app/src/main/java/com/example/gpssos/LoginActivity.java
+++ app/src/main/java/com/example/gpssos/LoginActivity.java
@@ -1,23 +1,38 @@
 package com.example.gpssos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button ingresar, registrarse;
+    private EditText etxtEmail, etxtPass;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
         ingresar = findViewById(R.id.btningresar);
         registrarse = findViewById(R.id.btnRegister);
+        etxtEmail = findViewById(R.id.etxt_email);
+        etxtPass = findViewById(R.id.etxt_password);
 
 
         //Ir al registro
@@ -41,7 +56,37 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void IniActivity(){
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
+        String email_User = etxtEmail.getText().toString().trim();
+        String pass_User = etxtPass.getText().toString().trim();
+
+        if (email_User.isEmpty() && pass_User.isEmpty()) {
+            Toast.makeText(LoginActivity.this, "Verifica que los datos esten completos", Toast.LENGTH_SHORT).show();
+        } else {
+            loginUser(email_User, pass_User);
+        }
+        //Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        //startActivity(intent);
     }
+
+    private void loginUser(String emailUser, String passUser) {
+        mAuth.signInWithEmailAndPassword(emailUser, passUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    finish();
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    Toast.makeText(LoginActivity.this, "Bienvenido", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 }
