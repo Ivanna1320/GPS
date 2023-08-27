e35478e04aaf8aaf85c18e6d20cd539648398ab4
ivanna <itorres13@ucol.mx>
2023-08-26 21:41:10 -0600

    Visualizacion de los datos del perfil

--- app/src/main/java/com/example/gpssos/PerfilFragment.java
+++ app/src/main/java/com/example/gpssos/PerfilFragment.java
@@ -4,35 +4,39 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.concurrent.Executor;
 
 
 public class PerfilFragment extends Fragment {
 
     private Button aggContacto, btnCerrar;
+    private TextView txtnameUser, txtidentificadorUser, txtEmailUser, txtCelularUser;
     FirebaseAuth mAuth;
+    FirebaseFirestore mFirestore;
+
 
     public PerfilFragment() {
         // Required empty public constructor
     }
-
-
-    /*public static PerfilFragment newInstance(String param1, String param2) {
-        PerfilFragment fragment = new PerfilFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }*/
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,8 +51,62 @@ public class PerfilFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_perfil, container, false);
 
         mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
+        //UserId = mAuth.getCurrentUser().getUid();
+        FirebaseUser user = mAuth.getCurrentUser();
+        String id = user.getUid();
+
         aggContacto = view.findViewById(R.id.agg_contac); // Asignar el botón por su ID
         btnCerrar = view.findViewById(R.id.btn_cerrar);
+        txtnameUser = view.findViewById(R.id.textname);
+        txtidentificadorUser = view.findViewById(R.id.textid);
+        txtEmailUser = view.findViewById(R.id.txt_email);
+        txtCelularUser = view.findViewById(R.id.txt_cel);
+
+        txtEmailUser.setText(mAuth.getCurrentUser().getEmail());
+
+        mFirestore.collection("user").document(id).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if (documentSnapshot.exists()) {
+                    String nombreUser = documentSnapshot.getString("nombre");
+                    String identificadorUser = documentSnapshot.getString("identificacion");
+                    String emailUser = documentSnapshot.getString("email");
+                    String celularUser = documentSnapshot.getString("celular");
+
+                    txtnameUser.setText(nombreUser);
+                    txtidentificadorUser.setText(identificadorUser);
+                    txtEmailUser.setText(emailUser);
+                    txtCelularUser.setText(celularUser);
+                }
+            }
+        });
+
+        /*mFirestore.collection("user").document(id).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    String nombreUser = documentSnapshot.getString("nombre");
+                    String identificadorUser = documentSnapshot.getString("identificacion");
+                    String emailUser = documentSnapshot.getString("email");
+                    String celularUser = documentSnapshot.getString("celular");
+
+                    txtnameUser.setText(nombreUser);
+                    txtidentificadorUser.setText(identificadorUser);
+                    txtEmailUser.setText(emailUser);
+                    txtCelularUser.setText(celularUser);
+                }
+            }
+        });*/
+        /*documentReference.addSnapshotListener((Executor) this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                nameUser.setText(documentSnapshot.getString("nombre"));
+                identidicadorUser.setText(documentSnapshot.getString("identificacion"));
+                txtEmailUser.setText(documentSnapshot.getString("email"));
+                txtCelularUser.setText(documentSnapshot.getString("celular"));
+            }
+        });*/
 
         aggContacto.setOnClickListener(new View.OnClickListener() {
             @Override
